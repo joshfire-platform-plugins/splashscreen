c840805bb66d277bf82a97050c489cedc4d02bb9
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-07-25 13:20:03 +0200

    Add factory style

--- loaded.js
+++ loaded.js
@@ -2,7 +2,17 @@ define([], function () {
   return function (params) {
     return {
       generate: function (options, callback) {
-        document.getElementsByClassName('_joshfire_factory_splashscreen')[0].style.display = 'none';
+        try {
+          var elt = document.getElementsByClassName('_joshfire_factory_splashscreen');
+          if (elt && elt.length) {
+            //TODO detect that the browser won't support opacity
+            elt[0].style.opacity = '0';
+            setTimeout(function() {
+              elt[0].style.display = 'none';
+            },700);
+          }
+
+        } catch (e) {}
         callback();
       }
     };
